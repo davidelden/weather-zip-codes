fd80889919f0e54738dcf5d41f05ca2efcb52856
davidelden <deelden+github@gmail.com>
2019-10-30 12:49:15 -0500

    add knexfile.js with db config

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+module.exports = {
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: 'weather_zip_codes'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  },
+  seeds: {
+    directory: './seeds/dev'
+  }
+};
