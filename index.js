d92f525f7508fba8343b22362fb02cc9539856d6
davidelden <deelden+github@gmail.com>
2019-10-30 13:05:14 -0500

    add migrateLatest to index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const readStream = require('./src/streams/actions/readStream.js'),
+const migrateLatest = require('./src/db/migrateLatest.js'),
+      readStream = require('./src/streams/actions/readStream.js'),
       msgEmitter = require('./src/emitter/msgEmitter.js'),
       streamName = 'StartWeatherFetch';
 
+migrateLatest();
 readStream(streamName);
 msgEmitter.on('streamMessage', msg => console.log('[ZipCodes]', msg));
 // msgEmitter.on('streamMessage', msg => publishZipCodes(msg));
