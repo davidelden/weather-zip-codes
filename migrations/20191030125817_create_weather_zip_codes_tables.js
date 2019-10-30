f6c1928a68bef5f402eda725b82d3a82497cd84f
davidelden <deelden+github@gmail.com>
2019-10-30 13:12:11 -0500

    change table name from weather_zip_codes -> zip_codes

--- migrations/20191030125817_create_weather_zip_codes_tables.js
+++ migrations/20191030125817_create_weather_zip_codes_tables.js
@@ -1,30 +1,30 @@
 exports.up = function(knex) {
   return knex.schema
-    .createTable('weather_zip_codes', tbl => {
+    .createTable('zip_codes', tbl => {
       tbl.increments('id').primary();
       tbl.string('zip_code').index().unique();
       tbl.timestamps(true, true);
     })
     .createTable('us_eastern', tbl => {
-      tbl.inherits('weather_zip_codes');
+      tbl.inherits('zip_codes');
     })
     .createTable('us_central', tbl => {
-      tbl.inherits('weather_zip_codes');
+      tbl.inherits('zip_codes');
     })
     .createTable('us_mountain', tbl => {
-      tbl.inherits('weather_zip_codes');
+      tbl.inherits('zip_codes');
     })
     .createTable('us_arizona', tbl => {
-      tbl.inherits('weather_zip_codes');
+      tbl.inherits('zip_codes');
     })
     .createTable('us_pacific', tbl => {
-      tbl.inherits('weather_zip_codes');
+      tbl.inherits('zip_codes');
     })
     .createTable('us_alaska', tbl => {
-      tbl.inherits('weather_zip_codes');
+      tbl.inherits('zip_codes');
     })
     .createTable('us_hawaii', tbl => {
-      tbl.inherits('weather_zip_codes');
+      tbl.inherits('zip_codes');
     })
 };
 
@@ -37,5 +37,5 @@ exports.down = function(knex) {
     .dropTable('us_pacific')
     .dropTable('us_alaska')
     .dropTable('us_hawaii')
-    .dropTable('weather_zip_codes')
+    .dropTable('zip_codes')
 };
