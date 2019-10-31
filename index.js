ec1ca1c5c913884661a6f79562a09e8809ec4f23
davidelden <deelden+github@gmail.com>
2019-10-31 11:40:29 -0500

    add publishZipCodes to index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const migrateLatest = require('./src/db/migrateLatest.js'),
       readStream = require('./src/streams/actions/readStream.js'),
       msgEmitter = require('./src/emitter/msgEmitter.js'),
+      publishZipCodes = require('./src/publish/publishZipCodes.js'),
       streamName = 'StartWeatherFetch';
 
 migrateLatest();
 readStream(streamName);
-msgEmitter.on('streamMessage', msg => console.log('[ZipCodes]', msg));
-// msgEmitter.on('streamMessage', msg => publishZipCodes(msg));
+msgEmitter.on('streamMessage', msg => publishZipCodes(msg));
